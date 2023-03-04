9ac9b05237aeafcae58058c4af4a922ea0333de7
Yasindu-Kumara <IT20299620@my.sliit.lk>
2023-03-04 09:28:37 +0530

    Final Code add

--- App.js
+++ App.js
@@ -5,54 +5,54 @@ console.log(data);
 });
 
 // const fs = require('fs');
-// fs.writeFile('file.txt', 'Hello World!', function (err) {
-// if (err) throw err;
-// console.log('File saved!');
-// });
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+if (err) throw err;
+console.log('File saved!');
+});
 
 
 // const fs = require('fs');
-// fs.writeFile('file.txt', 'Hello World!', function (err) {
-// if (err) throw err;
-// console.log('File saved!');
-// });
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+if (err) throw err;
+console.log('File saved!');
+});
 
-// const https = require('https');
-// https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-// let data = '';
-// resp.on('data', (chunk) => {
-// data += chunk;
-// });
-// resp.on('end', () => {
-// console.log(JSON.parse(data));
-// });
-// }).on('error', (err) => {
-// console.log("Error: " + err.message);
-// });
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+let data = '';
+resp.on('data', (chunk) => {
+data += chunk;
+});
+resp.on('end', () => {
+console.log(JSON.parse(data));
+});
+}).on('error', (err) => {
+console.log("Error: " + err.message);
+});
 
 // const myModule = require('./my-module.js');
 // console.log(myModule.myFunction());
 
 
-// const myPromise = new Promise((resolve, reject) => {
-// if (condition) {
-// resolve('Success!');
-// } else {
-// reject('Failure!');
-// }
-// });
-// myPromise.then((result) => {
-// console.log(result);
-// }).catch((error) => {
-// console.log(error);
-// });
+const myPromise = new Promise((resolve, reject) => {
+if (condition) {
+resolve('Success!');
+} else {
+reject('Failure!');
+}
+});
+myPromise.then((result) => {
+console.log(result);
+}).catch((error) => {
+console.log(error);
+});
 
-// async function myFunction() {
-// try {
-// const result = await myPromise;
-// console.log(result);
-// } catch (error) {
-// console.log(error);
-// }
-// }
-// myFunction();
+async function myFunction() {
+try {
+const result = await myPromise;
+console.log(result);
+} catch (error) {
+console.log(error);
+}
+}
+myFunction();
