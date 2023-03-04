a498e0be1dd77c106e5082d422e3a83f91dce420
Yasindu-Kumara <IT20299620@my.sliit.lk>
2023-03-04 09:14:37 +0530

    this is edit text file

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
+// const fs = require('fs');
+// fs.readFile('file.txt', 'utf8', function (err, data) {
+// if (err) throw err;
+// console.log(data);
+// });
+
+// const fs = require('fs');
+// fs.writeFile('file.txt', 'Hello World!', function (err) {
+// if (err) throw err;
+// console.log('File saved!');
+// });
+
+
 const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
+fs.writeFile('file.txt', 'Hello World!', function (err) {
 if (err) throw err;
-console.log(data);
+console.log('File saved!');
 });
-
-// const fs = require('fs');
-// fs.writeFile('file.txt', 'Hello World!', function (err) {
-// if (err) throw err;
-// console.log('File saved!');
-// });
-
-
-// const fs = require('fs');
-// fs.writeFile('file.txt', 'Hello World!', function (err) {
-// if (err) throw err;
-// console.log('File saved!');
-// });
 
 // const https = require('https');
 // https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
